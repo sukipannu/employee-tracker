c55eef85eac115f8e1e52771ad8a12b2db4bf1eb
sukipannu <sukipannu@ymail.com>
2022-11-27 15:29:40 -0500

    added functions to view employees

--- utils/init.js
+++ utils/init.js
@@ -58,3 +58,97 @@ function mainMenu() {
     });
 }
 
+// VIEW DEPARTMENTS
+function vDepts() {
+  const sql = `SELECT dept_id AS id, department_name AS name FROM dept`;
+  db.query(sql, (err, rows) => {
+    if (err) {
+      console.log(err.message);
+    }
+    console.log("");
+    console.table(rows);
+    setTimeout(() => {
+      mainMenu();
+    }, 750);
+  });
+}
+
+// VIEW ROLES
+function vRoles() {
+  const sql = `SELECT role_id AS id, role_title AS title, department_name AS name, role_salary AS salary FROM emp_role
+  LEFT JOIN dept ON emp_role.department_id = dept.dept_id`;
+  db.query(sql, (err, rows) => {
+    if (err) {
+      console.log(err.message);
+    }
+    console.log("");
+    console.table(rows);
+    setTimeout(() => {
+      mainMenu();
+    }, 800);
+  });
+}
+
+// VIEW EMPLOYEES
+function vEmploy() {
+  const sql = `SELECT e.emp_id AS id, concat(e.first_name,' ', e.last_name) AS employee, e.role_title AS title, e.role_salary AS salary, e.department_name AS dept,
+  CASE WHEN e.manager_id = e.emp_id THEN concat('N/A') ELSE concat(m.first_name, ' ', m.last_name) END AS manager FROM (SELECT * FROM employees LEFT JOIN emp_role ON employees.r_id = emp_role.role_id LEFT JOIN dept ON emp_role.department_id = dept.dept_id) AS e, employees m WHERE m.emp_id = e.manager_id`;
+  db.query(sql, (err, rows) => {
+    if (err) {
+      console.log(err.message);
+    }
+    console.table(rows);
+    setTimeout(() => {
+      mainMenu();
+    }, 800);
+  });
+}
+
+// VIEW EMPLOYEES BY DEPARTMENT
+function vEmployDept() {
+  const getDepartments = new Promise((resolve, reject) => {
+    var departmentsArr = [];
+    const sql = `SELECT department_name FROM dept`;
+    db.query(sql, (err, rows) => {
+      if (err) {
+        console.log(err.message);
+      }
+      // ROLE FOR LOOP
+      for (var i = 0; i < rows.length; i++) {
+        departmentsArr.push(Object.values(rows[i])[0]);
+      }
+      resolve(departmentsArr);
+    });
+  });
+  getDepartments.then((departmentsArr) => {
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "departmentId",
+          message: "Choose the department of your role",
+          choices: departmentsArr,
+          filter: (deptIdInput) => {
+            if (deptIdInput) {
+              return departmentsArr.indexOf(deptIdInput);
+            }
+          },
+        },
+      ])
+      .then(({ departmentId }) => {
+        const sql = ` SELECT e.emp_id as id, concat(e.first_name,' ', e.last_name) AS employee, e.role_title AS title, e.role_salary AS salary, e.department_name AS dept, 
+          CASE WHEN e.manager_id = e.emp_id THEN concat('N/A') ELSE concat(m.first_name, ' ', m.last_name) END AS manager 
+          FROM (SELECT * FROM employees LEFT JOIN emp_role ON employees.r_id = emp_role.role_id LEFT JOIN dept ON emp_role.department_id = dept.dept_id) AS e, employees m 
+          WHERE m.emp_id = e.manager_id
+          AND dept_id = ? `;
+        const query = [departmentId + 1];
+        db.query(sql, query, (err, rows) => {
+          if (err) {
+            console.log(err.message);
+          }
+          console.table(rows);
+          mainMenu();
+        });
+      });
+  });
+}
