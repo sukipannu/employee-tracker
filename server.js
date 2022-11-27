10978c236cd91d9e3067194a45f6f38c7cfee50a
sukipannu <sukipannu@ymail.com>
2022-11-27 16:37:52 -0500

    adding connect to db

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const db = require("./db/connect.js");
 const init = require("./utils/init.js");
+const mysql = require('mysql2');
+
 
 db.connect((err) => {
   if (err) throw err;
@@ -9,3 +11,15 @@ db.connect((err) => {
   }, 500);
 });
 
+// Connect to database
+// const db = mysql.createConnection(
+//   {
+//     host: 'localhost',
+//     // Your MySQL username,
+//     user: 'root',
+//     // Your MySQL password
+//     password: 'Summer123!',
+//     database: 'employeeDB'
+//   },
+//   console.log('Connected to the employee database.')
+// );
